55f819dd79932725bf8a4858736bae857109f398
朱秋柱 <zhuqiuzhu2013@gmail.com>
2017-04-27 17:42:53 +0800

    first commit

--- my_study_litho/src/main/java/zonda/exercise/litho/data/PostListConstants.java
+++ my_study_litho/src/main/java/zonda/exercise/litho/data/PostListConstants.java
@@ -6,7 +6,7 @@ package zonda.exercise.litho.data;
 
 public class PostListConstants {
 
-
+    public final static String TEST_API = "http://121.9.213.58/dis.myzaker.com/api/get_post.php?_appid=AndroidPhone&_bsize=720_1280&_city=%E5%B9%BF%E5%B7%9E&_dev=849&_lat=23.153452&_lbs_city=%E5%B9%BF%E5%B7%9E&_lbs_province=%E5%B9%BF%E4%B8%9C%E7%9C%81&_lng=113.353047&_mac=04%3A02%3A1f%3A1f%3A73%3A2b&_mcode=17A4EC1C&_net=wifi&_nudid=fb4af9250044fba6&_os=4.4.2_CHE-TL00&_os_name=CHE-TL00&_province=%E5%B9%BF%E4%B8%9C%E7%9C%81&_testuser=1&_udid=866939022168787&_v=7.8&_version=7.8&discussion_id=161&nocache=1&sort_type=last_comment";
 
     public final static String LOG_TAG = "PostList";
 }
